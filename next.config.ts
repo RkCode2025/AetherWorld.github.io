c26d977932857524b72b631ef5f90004d1930624
RKCODE2025 <ss3240069@gmail.com>
2025-03-13 11:22:43 +0530

    he

--- next.config.ts
+++ next.config.ts
@@ -3,14 +3,13 @@ import { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
-    unoptimized: true,  // You can disable image optimization if necessary
+    unoptimized: true,  // Optional, disable image optimization if necessary
   },
   basePath: '/AetherWorld.github.io',  // Set base path for GitHub Pages
-  trailingSlash: true,  // Optional, useful for GitHub Pages
+  trailingSlash: true,  // Useful for GitHub Pages to ensure correct URL structure
 
   // Set the output for static export
-  output: 'export',  // This enables exporting the app as static files
+  output: 'export',  // Enables exporting the app as static files
 };
 
 export default nextConfig;
-
